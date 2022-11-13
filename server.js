74bfa5855b6a12f4499c26df21738990b8f5eef1
CRgarcia15 <cg58708@gmial.com>
2022-11-13 14:58:28 -0500

    index.js => server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+//DEPENDENCIES
+const express = require('express')
+const app = express()
+
+//MIDDLEWARE
+app.use(express.json())
+
+//ROUTES
+app.get("/", (req, res) => {
+    res.send("You are in the home page")
+})
+
+app.get("*", (req, res) => {
+    res.status(404).send("404 | page not found")
+})
+
+const PORT = process.env.PORT || 7000
+
+//LISTENER
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
