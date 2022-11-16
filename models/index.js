dae2b9a2f142303e6f3cca92a09af7b70b60e5b3
CRgarcia15 <cg58708@gmial.com>
2022-11-16 15:43:55 -0500

    connected to mongodb

--- models/index.js
+++ models/index.js
@@ -1,6 +1,12 @@
 require("dotenv").config()
 const mongoose = require("mongoose")
-//get the mongo link 
-mongoose.connect(
 
+mongoose.connect(
+    "mongodb+srv://Crgarcia15:prkXlySkbbhMSRmu@cluster0.kczzd.mongodb.net/test",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
 )
+
+module.exports.Sprint = require("./sprint")
