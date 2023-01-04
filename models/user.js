f056aef7991d21a44eaa4b5d7aa3f3364affe00a
CRgarcia15 <cg58708@gmial.com>
2023-01-04 11:04:10 -0500

    Username is falsely required

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 
 //SCHEMA
  const userSchema = new mongoose.Schema({
-    name: {type: String},
+    name: {type: String, required: false},
     username: {type: String, required: true},
     password: {type: String, required: true}
  })
