ebd44f61787bf2eab7e9c21df268ca4a88ea7322
CRgarcia15 <cg58708@gmial.com>
2025-09-15 13:45:13 -0400

    updated nodes to newer versions and corrected issues that came from syntax not corresponding to the new versions of said nodes

--- models/index.js
+++ models/index.js
@@ -3,15 +3,14 @@ require("dotenv").config()
 const mongoose = require("mongoose")
 
 //MONGO CONNECTION
-mongoose.connect(
-    process.env.MONGO_URI,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }
-)
-    .then(() => console.log('DB Connected'))
-    .catch(err => console.error(err))
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log('DB Connected')
+})
+.catch(err => console.error(err))
 
 module.exports.Project = require("./project").default
 module.exports.User = require("./user")
--- server.js
+++ server.js
@@ -1,38 +1,47 @@
-//DEPENDENCIES
+// DEPENDENCIES
 const express = require('express')
-const app = express()
 const mongoose = require("mongoose")
 require("dotenv").config()
-const userRoutes =  require('./controllers/user')
-const projectRoutes = require('./controllers/projects')
-const authMiddleware = require('./middleware/auth')
 const cors = require('cors')
+
+// ROUTES
+const userRoutes = require('./controllers/user')
+const projectRoutes = require('./controllers/projects')
+
+// MIDDLEWARE
+const authMiddleware = require('./middleware/auth')
 const defineCurrentUser = require('./middleware/defineCurrentUser')
 
-//MIDDLEWARE
+const app = express()
+
+// EXPRESS MIDDLEWARE
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors({origin:'*'}))
+app.use(cors({ origin: '*' }))
 app.use(defineCurrentUser)
 
-//ROUTES
+// HOME ROUTE
 app.get("/", (req, res) => {
     res.send("You are in the home page")
 })
 
-//ROUTES
+// API ROUTES
 app.use("/user", userRoutes)
 app.use("/project", authMiddleware.validateJWT, projectRoutes)
 
-//ERROR HANDALING / 404
-app.get("*", (req, res) => {
+// CATCH-ALL 404 ROUTE
+app.use((req, res) => {
     res.status(404).send("404 | page not found")
 })
 
-//PORT THAT IS BEING USED
-const PORT = process.env.PORT
+// MONGO CONNECTION
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true
+})
+    .catch(err => console.error(err))
 
-//LISTENER
+// PORT LISTENER
+const PORT = process.env.PORT || 7000
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 })
